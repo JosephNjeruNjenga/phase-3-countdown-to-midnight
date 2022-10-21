1ba9534887d1d33606f1dd16c25e70e24d3eb1c8
+ <+>
2022-10-21 17:56:42 +0300

    Completed assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
+require 'pry'
 
-def countdown
+def countdown intgr
+    i = 0
+    while i < intgr
+        puts "#{intgr - i} SECOND(S)!"
+        i += 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep intgr 
+    sleep(intgr)
 end
